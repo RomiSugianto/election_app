e8b29862c5443d199546359bc03019ada95a4b79
watchths <watchths@gmail.com>
2019-04-26 01:41:28 +0700

    update admin

--- dialog_admin.cpp
+++ dialog_admin.cpp
@@ -4,6 +4,7 @@
 #include "QtSql/QSqlDatabase"
 #include "QtSql/QSqlQuery"
 #include "mainwindow.h"
+#include "QTimer"
 
 Dialog_admin::Dialog_admin(QWidget *parent) :
     QDialog(parent),
@@ -27,51 +28,6 @@ void Dialog_admin::on_pushButton_show_clicked()
     QString query_na = "SELECT COUNT(choice_id) AS total  FROM settle WHERE choice_id = 2;";
     QString query_nsy = "select count(szName) as total from `user` where iStatus = 0;";
     QString query_settle = "select count(szName) as total from `user` where iStatus = 1;";
-
-//    mysqlQuery.exec(query_jp);
-//    mysqlQuery.exec(query_na);
-//    mysqlQuery.exec(query_nsy);
-//    mysqlQuery.exec(query_settle);
-
-//    while(mysqlQuery.next())
-//    {
-//        total_jp = mysqlQuery.value("total").toInt();
-//        if(mysqlQuery.size() > 0)
-//        {
-//            ui->lcdNumber_jp->display(QString::number(total_jp));
-//        }
-//        break;
-//    }
-
-//    while(mysqlQuery.next())
-//    {
-//        total_jp = mysqlQuery.value("total").toInt();
-//        if(mysqlQuery.size() > 0)
-//        {
-//            ui->lcdNumber_na->display(QString::number(total_jp));
-//        }
-//        break;
-//    }
-
-//    while(mysqlQuery.next())
-//    {
-//        total_nsy = mysqlQuery.value("total").toInt();
-//        if(mysqlQuery.size() > 0)
-//        {
-//            ui->lcdNumber_nsy->display(QString::number(total_nsy));
-//        }
-//        break;
-//    }
-
-//    while(mysqlQuery.next())
-//    {
-//        total_settle = mysqlQuery.value("total").toInt();
-//        if(mysqlQuery.size() > 0)
-//        {
-//            ui->lcdNumber_settle->display(QString::number(total_settle));
-//        }
-//        break;
-//    }
 
     if(mysqlQuery.exec(query_jp))
     {
@@ -112,6 +68,7 @@ void Dialog_admin::on_pushButton_show_clicked()
             break;
         }
     }
+    ui->progressBar->setValue(100);
     if(mysqlQuery.exec(query_settle))
     {
         while(mysqlQuery.next())
@@ -120,7 +77,6 @@ void Dialog_admin::on_pushButton_show_clicked()
             if(mysqlQuery.size() > 0)
             {
                 ui->lcdNumber_settle->display(QString::number(total_settle));
-                ui->progressBar->setValue(100);
                 ui->progressBar->hide();
             }
             break;
